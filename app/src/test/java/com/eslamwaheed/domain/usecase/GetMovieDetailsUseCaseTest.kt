0a16825e1f4860310e62f44cc89f8560f55ce436
eslamwaheed <eslamwaheedelbadry@gmail.com>
2024-03-04 12:02:33 +0200

    add unit test for domain usecases

--- app/src/test/java/com/eslamwaheed/domain/usecase/GetMovieDetailsUseCaseTest.kt
+++ app/src/test/java/com/eslamwaheed/domain/usecase/GetMovieDetailsUseCaseTest.kt
@@ -1,5 +1,52 @@
 package com.eslamwaheed.domain.usecase
 
-import org.junit.jupiter.api.Assertions.*
+import com.appmattus.kotlinfixture.decorator.nullability.NeverNullStrategy
+import com.appmattus.kotlinfixture.decorator.nullability.nullabilityStrategy
+import com.appmattus.kotlinfixture.decorator.optional.optionalStrategy
+import com.appmattus.kotlinfixture.kotlinFixture
+import com.eslamwaheed.domain.model.moviedetails.MovieDetails
+import com.eslamwaheed.domain.repo.MoviesRepo
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-class GetMovieDetailsUseCaseTest
+@RunWith(JUnit4::class)
+class GetMovieDetailsUseCaseTest {
+    private val fixture = kotlinFixture {
+        nullabilityStrategy(NeverNullStrategy)
+        optionalStrategy { NeverNullStrategy }
+        repeatCount { 2 }
+    }
+
+    @MockK
+    private lateinit var moviesRepo: MoviesRepo
+
+    private lateinit var classUnderTest: GetMovieDetailsUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        classUnderTest = GetMovieDetailsUseCase(moviesRepo)
+    }
+
+    @Test
+    fun `invoke when success then return success of movieDetails`() = runTest {
+        val movieDetails: MovieDetails = fixture()
+
+        val movieID: Int = fixture()
+
+        val expectedResult = Result.success(movieDetails)
+
+        coEvery { moviesRepo.getMovieDetails(movieID) } returns expectedResult
+
+        val result = classUnderTest.invoke(movieID)
+
+        Assert.assertEquals(expectedResult, result)
+    }
+}
--- app/src/test/java/com/eslamwaheed/domain/usecase/GetNowPlayingListUseCaseTest.kt
+++ app/src/test/java/com/eslamwaheed/domain/usecase/GetNowPlayingListUseCaseTest.kt
@@ -1,5 +1,50 @@
 package com.eslamwaheed.domain.usecase
 
-import org.junit.jupiter.api.Assertions.*
+import com.appmattus.kotlinfixture.decorator.nullability.NeverNullStrategy
+import com.appmattus.kotlinfixture.decorator.nullability.nullabilityStrategy
+import com.appmattus.kotlinfixture.decorator.optional.optionalStrategy
+import com.appmattus.kotlinfixture.kotlinFixture
+import com.eslamwaheed.domain.model.nowplaying.NowPlaying
+import com.eslamwaheed.domain.repo.MoviesRepo
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-class GetNowPlayingListUseCaseTest
+@RunWith(JUnit4::class)
+class GetNowPlayingListUseCaseTest{
+    private val fixture = kotlinFixture {
+        nullabilityStrategy(NeverNullStrategy)
+        optionalStrategy { NeverNullStrategy }
+        repeatCount { 2 }
+    }
+
+    @MockK
+    private lateinit var moviesRepo: MoviesRepo
+
+    private lateinit var classUnderTest: GetNowPlayingListUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        classUnderTest = GetNowPlayingListUseCase(moviesRepo)
+    }
+
+    @Test
+    fun `invoke when success then return success of List of NowPlaying`() = runTest {
+        val listOfNowPlaying: List<NowPlaying> = fixture()
+
+        val expectedResult = Result.success(listOfNowPlaying)
+
+        coEvery { moviesRepo.getNowPlaying() } returns expectedResult
+
+        val result = classUnderTest.invoke()
+
+        Assert.assertEquals(expectedResult, result)
+    }
+}
